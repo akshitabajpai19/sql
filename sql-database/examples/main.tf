deb0af3f9201fd5bc4ad6fc2a82817461ae8b2f5
akshita-lumen <akshita.bajpai@lumen.com>
2024-09-25 21:37:20 +0530

    adding code for sql database

---
+++ sql-database/examples/main.tf
@@ -0,0 +1,21 @@
+module "sql-database" {
+  source              = "../"
+  database_names      = var.database_names
+  location            = var.location
+  resourcetype        = var.resourcetype
+  appid               = var.appid
+  appname             = var.appname
+  env                 = var.env
+  costCostCenter      = var.costCostCenter
+  costVP              = var.costVP
+  costAppOwnerTech    = var.costAppOwnerTech
+  costAppOwnerManager = var.costAppOwnerManager
+  costBudgetOwner     = var.costBudgetOwner
+  additionalcontext   = var.additionalcontext
+  costDivision        = var.costDivision
+  costAllocation      = var.costAllocation
+  appFunction         = var.appFunction
+  monthlyBudget       = var.monthlyBudget
+  costbaseline        = var.costbaseline
+
+}
