43e6f15aa058b2fe2b7af1eeda3762ed6874019e
akshitabajpai19 <80335201+akshitabajpai19@users.noreply.github.com>
2024-10-07 21:10:31 +0530

    kjhgugjkj

--- postgre-sql/providers.tf
+++ postgre-sql/providers.tf
@@ -1,14 +1,11 @@
 # tflint-ignore: terraform_required_providers
 terraform {
-  required_version = "~> 1.8"
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 4.3.0"
+      source = "hashicorp/azurerm"
     }
     random = {
-      source  = "hashicorp/random"
-      version = "3.5.1"
+      source = "hashicorp/random"
     }
     azapi = {
       source = "Azure/azapi"
@@ -20,11 +17,3 @@ terraform {
   }
 }
 
-provider "azurerm" {
-  features {
-    resource_group {
-      prevent_deletion_if_contains_resources = false
-    }
-  }
-  subscription_id = "8c5a7b36-81fd-4f19-a1f4-9cdb2a790f8e"
-}
